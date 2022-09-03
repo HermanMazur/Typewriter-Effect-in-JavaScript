67d6e20001d9564681f6fd11bbe368f0575591ff
color <95304946+HermanMazur@users.noreply.github.com>
2022-09-03 13:07:44 +0300

    => and <=

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const phrases = ['Hello.', 'I am Herman.', 'Bye.'];
 let i = 0;
 let j = 0;
 let currentPhrase = [];
+let isDeleting = false;
 
 function loop() {
     textDisplay.innerHTML = currentPhrase.join('');
@@ -10,18 +11,32 @@ function loop() {
     if (i < phrases.length) {
         // console.log(phrases[i])
 
-        if (j <= phrases[i].length) {
+        if (!isDeleting && j <= phrases[i].length) {
             // console.log(phrases[i][j])
             currentPhrase.push(phrases[i] [j])
             j += 1;
-
+            console.log('add a letter', j)
         }
+
+        if (isDeleting && j <= phrases[i].length) {
+            currentPhrase.pop(phrases[i][j])
+            j -= 1;
+            console.log('remove a letter', j)
+        }
+
         if (j == phrases[i].length) {
-            i  += 1;
-}
+            // i  += 1;
+            isDeleting = true;   
+        }
+
+        if (isDeleting && j === 0) {
+            currentPhrase = [];
+            isDeleting = false;
+            i += 1;
+        }
     }
 
-    setTimeout(loop, 500)
+    setTimeout(loop, 300)
 }
 
 loop();
